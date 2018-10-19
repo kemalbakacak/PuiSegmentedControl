2fffacb4cc9eaa118af82fb27eacaf966cc01aca
Kemal Bakacak <kemalbakacak10@gmail.com>
2018-10-19 23:43:58 +0300

    Created example project.

---
+++ Example/PuiSegmentedControlExample/PuiSegmentedControlExample/Source/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PuiSegmentedControlExample
+//
+//  Created by Kemal Bakacak on 19.10.2018.
+//  Copyright © 2018 Kemal Bakacak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
