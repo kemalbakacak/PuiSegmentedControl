05dacf595dfd14282b63db13347afd744407c1ea
Kemal Bakacak <kemalbakacak10@gmail.com>
2018-10-19 23:08:29 +0300

    Initial Commit

---
+++ PuiSegmentedControl/PuiSegmentedControl.h
@@ -0,0 +1,19 @@
+//
+//  PuiSegmentedControl.h
+//  PuiSegmentedControl
+//
+//  Created by Kemal Bakacak on 19.10.2018.
+//  Copyright © 2018 Kemal Bakacak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PuiSegmentedControl.
+FOUNDATION_EXPORT double PuiSegmentedControlVersionNumber;
+
+//! Project version string for PuiSegmentedControl.
+FOUNDATION_EXPORT const unsigned char PuiSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PuiSegmentedControl/PublicHeader.h>
+
+
