16190121c39b20f3a1196a35a2589dce3e9df6c2
Kemal Bakacak <kemalbakacak10@gmail.com>
2018-10-19 23:22:12 +0300

    Added framework's files.

---
+++ Source/PuiSegmentedControl.swift
@@ -0,0 +1,14 @@
+//
+//  PuiSegmentedControl.swift
+//  PuiSegmentedControl
+//
+//  Created by Kemal Bakacak on 19.10.2018.
+//  Copyright © 2018 Kemal Bakacak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class PuiSegmentedControl: UISegmentedControl {
+    
+}
